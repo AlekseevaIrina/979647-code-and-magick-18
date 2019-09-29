0dad6b88572c928c49054c3d8135bbfaa6deccfd
Irina Alekseeva <50372200+AlekseevaIrina@users.noreply.github.com>
2019-09-29 16:26:23 +0300

    Fixed errors

--- js/setup.js
+++ js/setup.js
@@ -2,8 +2,9 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var SHOWN_WIZARDS = 4;
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -20,11 +21,11 @@ var getRandomInt = function (max) {
 
 var createWizards = function () {
   var wizards = [];
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < SHOWN_WIZARDS; i++) {
     wizards.push({
       name: WIZARD_NAMES[getRandomInt(WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomInt(WIZARD_SURNAMES.length - 1)],
-      coatColor: WIZARD_COAT_COLOR[getRandomInt(WIZARD_COAT_COLOR.length - 1)],
-      eyesColor: WIZARD_EYES_COLOR[getRandomInt(WIZARD_EYES_COLOR.length - 1)]
+      coatColor: WIZARD_COAT_COLORS[getRandomInt(WIZARD_COAT_COLORS.length - 1)],
+      eyesColor: WIZARD_EYES_COLORS[getRandomInt(WIZARD_EYES_COLORS.length - 1)]
     });
   }
   return wizards;
